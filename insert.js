1ec66964704a806692c2808553010e4a4ad80f7f
Max Leibowitz <403089@bsd48.org>
2020-02-06 08:57:53 -0800

    Working on dark theme...  (trying to make it find all ul tags in tree)

--- insert.js
+++ insert.js
@@ -3,3 +3,24 @@ $.get('/index.html', null, function(text){
   element = $(text)[19]
   output.appendChild(element);
 });
+
+$.get('/index.html', null, function(text){
+    var output = document.getElementsByTagName("body");
+    console.log(output)
+    output[0].classList.add("bg-dark")
+    output[0].classList.add("text-light")
+});
+
+$.get('/index.html', null, function(text){
+    var output = $("div").find("ul")
+    console.log("aaa")
+    console.log(output)
+    console.log("aaa")
+    for (i of output) {
+        console.log(i)
+        if (!"bg" in i.classList) {
+            i.classList.add("bg-dark")
+            i.classList.add("text-light")
+        }
+    }
+});
