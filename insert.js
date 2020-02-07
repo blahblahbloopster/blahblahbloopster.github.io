a89e8a5ad2a6c64c20f6e0e3ed38c234dc57d550
Max Leibowitz <403089@bsd48.org>
2020-02-06 20:53:27 -0800

    Half-baked fix?

--- insert.js
+++ insert.js
@@ -1,29 +1,18 @@
 setTimeout(() => {
-// if ("<nav>" in document.querySelectorAll("*")) {
-//     console.log("AAAAAA")
-// }
-try {
-    var foo = document.getElementsByTagName("nav")
-    console.log(foo)
-} catch {
     $.get('/index.html', null, function(text){
-        var output = document.getElementById("navbar_box");
-        element = $(text)[19]
-        output.appendChild(element);
-      });
-}
-
-$.get('/index.html', null, function(text){
-    var output = document.getElementsByTagName("body");
-    output[0].classList.add("bg-dark")
-    output[0].classList.add("text-light")
-});
-
-var lst = document.querySelectorAll("*")
-for (i of lst) {
-    console.log(i.classList)
-    if (!"bg" in i.classList) {
+      var output = document.getElementById("navbar_box");
+      element = $(text)[19]
+      output.appendChild(element);
+    });
+    
+    $.get('/index.html', null, function(text){
+        var output = document.getElementsByTagName("body");
+        output[0].classList.add("bg-dark")
+        output[0].classList.add("text-light")
+    });
+    
+    var lst = document.querySelectorAll("*")
+    for (i of lst) {
         i.classList.add("bg-dark")
     }
-}
 }, 500);
