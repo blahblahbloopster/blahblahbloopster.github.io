44b786a2ed960efc5e1c6a962e7b68f86229c4a9
Max Leibowitz <403089@bsd48.org>
2020-02-10 07:46:23 -0800

    Fixed dark theme?  (again)

--- insert.js
+++ insert.js
@@ -1,9 +1,11 @@
 setTimeout(() => {
-    $.get('/index.html', null, function(text){
-      var output = document.getElementById("navbar_box");
-      element = $(text)[19]
-      output.appendChild(element);
-    });
+    if (document.location.pathname != "/index.html" & document.location.pathname != "/") {
+        $.get('/index.html', null, function(text){
+            var output = document.getElementById("navbar_box");
+            element = $(text)[21]
+            output.appendChild(element);
+        });
+    }
     
     $.get('/index.html', null, function(text){
         var output = document.getElementsByTagName("body");
@@ -12,15 +14,12 @@ setTimeout(() => {
     });
     
     for (i of document.getElementsByClassName("list-group")) {
-        console.log(i)
         i.classList.add("bg-dark")
     }
     for (i of document.getElementsByClassName("list-group-item")) {
-        console.log(i)
         i.classList.add("bg-dark")
     }
     for (i of document.getElementsByClassName("card")) {
-        console.log(i)
         i.classList.add("bg-dark")
     }
     }, 500);
